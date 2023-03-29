2aa0dd31a7d122469a9a16b6c3e439c4bb233dcc
sharvesh642 <117143719+sharvesh642@users.noreply.github.com>
2023-03-29 13:05:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ def add(number_a:int, number_b:int):
     """
     This function adds 2 numbers
     """
-    return number_a  + number_b 
+    print( number_a  + number_b )
     
+    
+"""
+There is no error in the code. But we can add a print inplace of return the show the output.
+
+"""
